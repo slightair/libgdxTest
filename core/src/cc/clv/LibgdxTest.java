fee14d52ba7a19667df13dec0c2659fb940973fb
slightair <arksutite@gmail.com>
2014-12-14 17:31:16 +0900

    Add GreenBox

--- core/src/cc/clv/LibgdxTest.java
+++ core/src/cc/clv/LibgdxTest.java
@@ -2,80 +2,80 @@ package cc.clv;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
 public class LibgdxTest extends ApplicationAdapter {
-    SpriteBatch batch;
-    BitmapFont font;
-    Texture img;
-    Sprite sprite;
-    Vector2 pos;
-    float scale;
-    float angle;
+    public Environment environment;
+    public PerspectiveCamera camera;
+
+    public CameraInputController cameraInputController;
+
+    public ModelBatch modelBatch;
+    public Model model;
+    public ModelInstance modelInstance;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        font = new BitmapFont();
-        sprite = new Sprite(img);
-        pos = new Vector2();
-        scale = 1.0f;
-        angle = 0.0f;
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        modelBatch = new ModelBatch();
+
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(10f, 10f, 10f);
+        camera.lookAt(0, 0, 0);
+        camera.near = 1f;
+        camera.far = 300f;
+        camera.update();
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                Usage.Position | Usage.Normal);
+        modelInstance = new ModelInstance(model);
+
+        cameraInputController = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(cameraInputController);
     }
 
     @Override
     public void render() {
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            pos.y += 1.0;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            pos.x -= 1.0;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            pos.y -= 1.0;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            pos.x += 1.0;
-        }
+        cameraInputController.update();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            angle += 0.5;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            angle -= 0.5;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            scale += 0.01;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            scale -= 0.01;
-        }
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        String info = String.format("pos(%f,%f)", pos.x, pos.y);
-        sprite.setPosition(pos.x, pos.y);
-        sprite.setScale(scale);
-        sprite.setRotation(angle);
-
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        sprite.draw(batch);
-        font.draw(batch, info, 0, 420);
-        font.draw(batch, "Hello LibGDX", 200, 400);
-        batch.end();
+        modelBatch.begin(camera);
+        modelBatch.render(modelInstance, environment);
+        modelBatch.end();
     }
 
     @Override
     public void dispose() {
-        font.dispose();
-        batch.dispose();
-        img.dispose();
+        modelBatch.dispose();
+        model.dispose();
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
     }
 }
